101ea23edea779fcb44e74488bd6243c41750044
Dedeepya <166736530+UnnamDedeepya@users.noreply.github.com>
2025-06-20 15:00:29 +0530

    Update Product.java

--- Week-1/Data Structures & Algorithms/InventoryManagement/src/Product.java
+++ Week-1/Data Structures & Algorithms/InventoryManagement/src/Product.java
@@ -4,25 +4,25 @@ package model;
  * Represents a single product in the inventory.
  */
 public class Product {
-    private String productId;
-    private String productName;
+    private String prodId;
+    private String prodName;
     private int quantity;
     private double price;
 
-    public Product(String productId, String productName, int quantity, double price) {
-        this.productId = productId;
-        this.productName = productName;
+    public Product(String prodId, String prodName, int quantity, double price) {
+        this.prodId = prodId;
+        this.prodName = prodName;
         this.quantity = quantity;
         this.price = price;
     }
 
     // Getters & Setters
     public String getProductId() {
-        return productId;
+        return prodId;
     }
 
     public String getProductName() {
-        return productName;
+        return prodName;
     }
 
     public int getQuantity() {
@@ -43,6 +43,6 @@ public class Product {
 
     @Override
     public String toString() {
-        return "[ID: " + productId + ", Name: " + productName + ", Qty: " + quantity + ", Price: ₹" + price + "]";
+        return "[ID: " + prodId + ", Name: " + prodName + ", Qty: " + quantity + ", Price: ₹" + price + "]";
     }
 }
